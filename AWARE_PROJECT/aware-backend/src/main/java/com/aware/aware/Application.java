3c432b831428c94652b8cce6745871cb99f58aec
OVERLORDxx <ks14635142@gmail.com>
2026-05-08 18:58:49 +0530

    Update Application.java

--- AWARE_PROJECT/aware-backend/src/main/java/com/aware/aware/Application.java
+++ AWARE_PROJECT/aware-backend/src/main/java/com/aware/aware/Application.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        String port = System.getenv("PORT");
 
+        if (port != null) {
+            System.setProperty("server.port", port);
+        }
+
+        SpringApplication.run(Application.class, args);
+    }
 }
